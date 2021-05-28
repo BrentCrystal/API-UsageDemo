c6088fa6ee84bd9476cc782fdb836e6203caaa8b
brent00mi <brentrcrystal@gmail.com>
2021-05-27 21:34:35 -0400

    Add Class Library Project
    Added DemoLibrary to API-UsageDemo Solution.

---
+++ DemoLibrary/ApiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace DemoLibrary
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient
+        }
+    }
+
+}
