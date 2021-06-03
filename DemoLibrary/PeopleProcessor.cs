74b8ecaff0ded63839df192133ddca60dab97bf8
brent00mi <brentrcrystal@gmail.com>
2021-06-02 20:17:43 -0400

    Added PeopleProcessor Class
    Class added to load people.

---
+++ DemoLibrary/PeopleProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class PeopleProcessor
+    {
+        public static async Task<PersonModel> LoadPerson(int personId = 0)
+        {
+            string url = "";
+
+            if (personId > 0)
+            {
+                url = $"https://swapi.dev/api/people/ {personId}/";
+            }
+            else
+            {
+                url = $"https://swapi.dev/";
+            }
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    PersonModel person = await response.Content.ReadAsAsync<PersonModel>();
+
+                    return person;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+
+        }
+    }
+}
