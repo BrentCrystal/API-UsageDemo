4a5d14e7856566c027548e2864647b16cc77cde9
brent00mi <brentrcrystal@gmail.com>
2021-06-02 20:18:45 -0400

    Added PersonModel
    Added PersonModel to map person properties.

---
+++ DemoLibrary/PersonModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibrary
+{
+    public class PersonModel
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Mass { get; set; }
+        public string Hair_Color { get; set; }
+        public string Skin_Color { get; set; }
+        public string Eye_Color { get; set; }
+        public string Birth_Year { get; set; }
+        public string Gender { get; set; }
+        public string HomeWorld { get; set; }
+        public string[] Films { get; set; }
+        public string[] Species { get; set; }
+        public string[] vehicles { get; set; }
+    }
+}
