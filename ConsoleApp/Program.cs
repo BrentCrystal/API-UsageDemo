f6d9b4d435ec7377fcd26a32ed4d509b379daf97
brent00mi <brentrcrystal@gmail.com>
2021-06-02 20:20:33 -0400

    Console and API Modifications
    Modifications to Console UI and API projects.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using DemoLibrary;
+using System;
+using System.Reflection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApiHelper.InitializeClient();
+
+            try
+            {
+                var person = PeopleProcessor.LoadPerson(32).Result;
+
+                Type t = person.GetType();
+                PropertyInfo[] pi = t.GetProperties();
+
+                foreach (PropertyInfo p in pi)
+                {
+                    Console.WriteLine(p.Name + " : " + p.GetValue(person));
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Person not found");
+            }
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
--- DemoLibrary/ApiHelper.cs
+++ DemoLibrary/ApiHelper.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Net.Http.Headers;
 
 namespace DemoLibrary
 {
-    public class ApiHelper
+    public static class ApiHelper
     {
         public static HttpClient ApiClient { get; set; }
 
         public static void InitializeClient()
         {
             ApiClient = new HttpClient();
+            //ApiClient.BaseAddress = new Uri("https://swapi.dev/");
             ApiClient.DefaultRequestHeaders.Accept.Clear();
-            ApiClient
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
     }
 
