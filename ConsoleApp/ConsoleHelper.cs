4c2ef8f51968a0c562c7e71d9e8c9134da726824
brent00mi <brentrcrystal@gmail.com>
2021-06-03 20:52:28 -0400

    Added ConsoleHelper Class
    Added class to send write console messages to user and obtain user input data.

---
+++ ConsoleApp/ConsoleHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public static class ConsoleHelper
+    {
+        public static int GetPersonId(this String message)
+        {
+            int output = 0;
+
+            Console.WriteLine(message);
+
+            bool isvalidInt = int.TryParse(Console.ReadLine(), out output);
+
+            while (isvalidInt == false)
+            {
+                Console.WriteLine(message);
+                isvalidInt = int.TryParse(Console.ReadLine(), out output);
+            }
+
+            return output;
+        }
+    }
+}
