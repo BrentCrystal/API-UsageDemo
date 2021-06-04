4c525c9b850e69f5cbaf96b4f419f2d64f643e2c
brent00mi <brentrcrystal@gmail.com>
2021-06-03 20:53:25 -0400

    Refactored Program.cs
    Made changes to Program.cs code.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -11,28 +11,13 @@ namespace ConsoleApp
         {
             ApiHelper.InitializeClient();
 
+            PersonModel person = new PersonModel();
+
+            int personId = "Enter an Id number to return Star Wars character data:".GetPersonId();
+
             try
             {
-                var person = PeopleProcessor.LoadPerson(1).Result;
-
-                Type t = person.GetType();
-                PropertyInfo[] properties = t.GetProperties();
-
-                foreach (PropertyInfo p in properties)
-                {
-                    if (p.PropertyType.IsArray )
-                    {
-                        Array a = (Array)p.GetValue(person);
-                        for (int i = 0; i < a.Length; i++)
-                        {
-                            Console.WriteLine(p.Name + " : " + a.GetValue(i));
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine(p.Name + " : " + p.GetValue(person));
-                    }
-                }
+                person = GetPersonData(personId);
                 
             }
             catch (Exception ex)
@@ -40,9 +25,39 @@ namespace ConsoleApp
                 Console.WriteLine("Person not found");
             }
 
+            Console.WriteLine();
+            PrintPersonData(person);
+
             Console.ReadLine();
         }
 
-       
+        private static void PrintPersonData(PersonModel person)
+        {
+            Type t = person.GetType();
+            PropertyInfo[] properties = t.GetProperties();
+
+            foreach (PropertyInfo p in properties)
+            {
+                if (p.PropertyType.IsArray)
+                {
+                    Array a = (Array)p.GetValue(person);
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        Console.WriteLine(p.Name + " : " + a.GetValue(i));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(p.Name + " : " + p.GetValue(person));
+                }
+            }
+        }
+
+        private static PersonModel GetPersonData(int personId)
+        {
+            var person = PeopleProcessor.LoadPerson(personId).Result;
+
+            return person;
+        }
     }
 }
